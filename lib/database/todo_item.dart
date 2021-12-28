fe9c32d666c7163c56f267481fc7bdfb490bcb65
Naral <you@example.com>
2021-12-28 22:17:11 +0700

    First Commit

---
+++ lib/database/todo_item.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+class ToDoItem {
+  static String table = 'todo';
+
+  final int id;
+  final String name;
+
+  ToDoItem({
+    required this.id,
+    required this.name,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+
+  factory ToDoItem.fromMap(Map<String, dynamic> map) {
+    return ToDoItem(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+    );
+  }
+}
