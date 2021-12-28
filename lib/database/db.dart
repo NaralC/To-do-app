7299244a3effb5bae881333ed9de29c3389a4484
Naral <you@example.com>
2021-12-28 22:55:14 +0700

    Update

---
+++ lib/database/db.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import '../database/todo_item.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as p;
+
+abstract class DB {
+  static Database? _db;
+  static int get _version => 1;
+
+  static Future<void> init() async {
+    //Create table
+    Future<void> onCreate(Database db, int version) async {
+      await db.execute(
+          'CREATE TABLE TODO (id INTEGER PRIMARY KEY NOT NULL, name STRING)');
+    }
+
+    //Fetch data
+    Future<List<Map<String, dynamic>>> query(String table) async {
+      return await _db!.query(table);
+    }
+
+    //Insert data
+    Future<int> insert(String table, ToDoItem todo) async {
+      return await _db!.insert(table, todo.toMap());
+    }
+
+    //Delete data
+    Future<int> delete(String table, ToDoItem todo) async {
+      return await _db!.delete(table, where: 'id = ?', whereArgs: [todo.id]);
+    }
+
+    try {
+      String _path = await getDatabasesPath();
+      String _databasepath = p.join(_path, 'todolist.db');
+
+      _db = await openDatabase(_databasepath,
+          version: _version, onCreate: onCreate);
+    } catch (exception) {
+      print(exception);
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
+import 'database/db.dart';
+import 'database/todo_item.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DB.init();
   runApp(const MyApp());
 }
 
@@ -10,6 +15,58 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<ToDoItem> _todo = [];
+  List<Widget>? get _items => _todo.map((item) => format(item)).toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+  Widget format(ToDoItem item) {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+        child: Dismissible(
+          key: Key(item.id.toString()),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Theme.of(context).primaryColor,
+                shape: BoxShape.rectangle,
+                boxShadow: const <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 10,
+                    offset: Offset(0, 10),
+                  )
+                ]),
+            child: Expanded(
+              child: Text(
+                item.name,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
